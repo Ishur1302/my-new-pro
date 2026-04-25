d89f5139423cef2b8466d0e84006657e479377d1
Ishur1302 <ishansh4420@gmail.com>
2026-04-25 22:15:17 +0530

    Real change this time

---
+++ ishur1.js
@@ -0,0 +1,58 @@
+//javascript is essential say because of it we have client side 
+//verification and so on it has VBscript and typescript which goes to JS anyway
+//it is for client to interact with browser
+//Why essential as it has direct interaction (allows di with webpages dynamically on the client)
+//Backwards compatible(all websites that worked before should work today as well)
+//js is central as without it alot of websites would stop working
+//browser speak js
+//ease of use easier than other PL 
+//JS has but
+//we use it in plain JS JS frameworks and node js reently dino
+//accesible and has a community 
+//adding an icon in online shopping 
+//it is used for power websites,communicate with db,native feel to web apps
+//react native and for IOT
+//so at first the coders had to write dif code for diff browsers
+//then a thing named JQuery came and it made it simpler
+//one another way was react which also made alot of things better
+//react made it even simpler alot of old code for JS is legacy code
+//first we need to learn plain JS then we will proceed to any react etc
+console.log(9);  
+"John"
+var p;
+p="ishu";
+console.log("Helo",p);
+var s="hello";
+console.log(s,p);
+p="ishurr";
+console.log(p);
+
+//same code diff op versatile
+/*
+String
+Number
+Boolean (T/F)
+Nul
+Undefined not been assigned a value
+BigInt greater range of no
+Symbol unique identifier mutipke boxes with same name butr diff serial no
++,-,*,/
+&&,||,!
+>,<,>=,<=,==,===
+all the data types of it comes under in number too
+like 123
+123.78
+10**2
+9%8
+*/
+console.log(3==2);
+let a=90;
+console.log(a);
+console.log(2*(4+8));
+console.log(" ");
+console.log('heyy cutieee');
+//console.log('It's) use double ""
+console.log(100=="100");
+console.log(100==="100"); //false as it is not same data type
+console.log(100!="100")
+console.log(100!=="100")
