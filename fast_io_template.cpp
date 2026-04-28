47f2117e318e0563579f957035eff68e9fb47a2d
Ishur1302 <ishansh4420@gmail.com>
2026-04-28 23:07:31 +0530

    feat: add fast I/O C++ template for competitive programming

---
+++ fast_io_template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Fast I/O Template
+ * Reduces overhead of cin/cout for large inputs (10^5+)
+ */
+void solve() {
+    // Your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
